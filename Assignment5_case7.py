e36a61efbc9428d926520cad32b97e82e5ada2d4
pallegithub <76815377+pallegithub@users.noreply.github.com>
2021-01-07 13:28:32 +0530

    Update Assignment5_case7.py

--- Assignment5_case7.py
+++ Assignment5_case7.py
@@ -1,16 +1,5 @@
-def tuple_min(tup):
-    if len(tup) == 1:
-        return tup[0]
-    elif len(tup) >= 2:
-        a = tup[0]
-        b = tuple_min(tup[1:])
-        return a if a<b else b
-length=int(input("Enter the length of the tuple==>"))
-list1=[]
-tuple1=tuple(list1)
-for i in range(length):
-    element=int(input("Enter the element==>"))
-    list1.append(element)
-print("")
-print("Minimum of the Elements in the tuple==>", tuple_min(tuple1)) 
-print("")
+def minimum_tuple(tup):
+    mini=min(tup)
+    print("Minimum in the tuple Elements",mini)
+tuple=(4,6,2,7,8,9,19)
+minimum_tuple(tuple)
